42f231a25f9d7a092fe69b3ef89e062800650103
Jake Flynn <jakepflynn@gmail.com>
2019-10-07 14:57:50 +0100

    added build

---
+++ build/precache-manifest.e07554789db4020789b213d593e90efb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f9f9566a6b550c38e6b9dd17f8bdff0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6fc3e766476b7360f85",
+    "url": "/static/js/2.83bc47c9.chunk.js"
+  },
+  {
+    "revision": "54bedbb9c9730c6e3c9a",
+    "url": "/static/js/main.d7f27821.chunk.js"
+  },
+  {
+    "revision": "d5ea2db11211f7a7efd7",
+    "url": "/static/js/runtime-main.f80fef8c.js"
+  }
+]);

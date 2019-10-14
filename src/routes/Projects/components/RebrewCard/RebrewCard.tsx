f941021e6c810ecd5d2d7e6e466347ea93aa4cb1
Jake Flynn <jakepflynn@gmail.com>
2019-10-14 09:48:24 +0100

    added link

--- src/routes/Projects/components/RebrewCard/RebrewCard.tsx
+++ src/routes/Projects/components/RebrewCard/RebrewCard.tsx
@@ -19,12 +19,13 @@ const RebrewCard: FC<IRebrewCardProps> = ({ classes }) => (
     <Text h2>Rebrew <span role="img" aria-label="beers">🍻</span></Text>
     <Text>Rebrew is an application I created to explore using TypeScript full stack.</Text>
     <Text>
-      On the back-end there is a GraphQL API built with Nexus & Prisma 2. To get the data, I consumed&nbsp;
-      <Link to="https://punkapi.com/documentation/v2">a third party API</Link> and extended it to support user accounts.
+      On the back-end there is a GraphQL API built with Prisma 2. To get the beer data, I consumed&nbsp;
+      <Link to="https://punkapi.com/documentation/v2">this API</Link> and extended it to support accounts.
     </Text>
     <Text>
-      The main purpose of the application is to search for a beer and leave a rating/review.
+      The main purpose of the app is to lookup/leave a review for a beer.
     </Text>
+    <Text caption>Please note that the public version of this app is using a sandbox plan for data storage, as such only 1 pool may be open at a time!</Text>
     <div>
       <ReactIcon />
       <TypeScriptIcon />
@@ -32,7 +33,7 @@ const RebrewCard: FC<IRebrewCardProps> = ({ classes }) => (
       <NodeIcon />
     </div>
     <div className={classes.linksWrapper}>
-      <Link to ="" icon={<FiExternalLink />}>
+      <Link to ="https://rebrew.jakepflynn.now.sh/" icon={<FiExternalLink />}>
         Open App
       </Link>
       <Link to="https://github.com/jakeswork/rebrew-server" icon={<FiCode />}>
